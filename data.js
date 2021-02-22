d1da1a579c68e459fe34bbbc09d9642e9795fead
malagorzata <gosia.warwaszynska@gmail.com>
2021-02-22 13:33:00 +0100

    filter level javascript working

--- data.js
+++ data.js
@@ -1,6 +1,17 @@
-const url = "https://web35mm-e688.restdb.io/rest/cameras?max=19";
-const mediaurl ="https://web35mm-e688.restdb.io/media";
+let url = "https://web35mm-e688.restdb.io/rest/cameras?max=19";
+const mediaurl = "https://web35mm-e688.restdb.io/media";
 
+document.querySelector("select.level").addEventListener("change", handleSelect);
+
+function handleSelect(evt) {
+  console.log(evt.target.value);
+  if (evt.target.value === "Show all") {
+    url = `https://web35mm-e688.restdb.io/rest/cameras?max=19`;
+  } else {
+    url = `https://web35mm-e688.restdb.io/rest/cameras?max=19&q={"level":"${evt.target.value}"}`;
+  }
+  getData();
+}
 /*API key*/
 const options = {
   headers: {
@@ -8,24 +19,29 @@ const options = {
   },
 };
 
-fetch(url, options)
-  .then((response) => {
-    if (!response.ok) {
-      throw Error(response.statusText);
-    }
-    return response.json();
-  })
+function getData() {
+  fetch(url, options)
+    .then((response) => {
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    })
 
-  .then((data) => {
-    // console.log(data);
-    handleData(data);
-  })
-  .catch((e) => {
-    console.error("an error occured:", e.message);
-  });
+    .then((data) => {
+      // console.log(data);
+      handleData(data);
+    })
+    .catch((e) => {
+      console.error("an error occured:", e.message);
+    });
+}
+
+getData();
 
 function handleData(data) {
   console.log(data);
+  document.querySelector(".products").innerHTML = "";
   data.forEach(showProduct);
 }
 
@@ -50,9 +66,9 @@ function showProduct(product) {
   copy.querySelector(".format").textContent = product.format;
   copy.querySelector(".level").textContent = product.level;
   copy.querySelector("img.cameraimg").src = "img";
-const imgurl = mediaurl + product.img[0];
-copy.querySelector(".cameraimg").src = imgurl;
-console.log(imgurl);
+  const imgurl = mediaurl + product.img[0];
+  copy.querySelector(".cameraimg").src = imgurl;
+  console.log(imgurl);
   //grab the parent
   const parent = document.querySelector(".products");
   //append
