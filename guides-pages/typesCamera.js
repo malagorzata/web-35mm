df57aba8d68257a73d8de973c5571a0c7acb1fa8
anasofich <castellanos_chopas@hotmail.com>
2021-02-22 23:46:17 +0100

    made types of camera page dynamic

--- guides-pages/typesCamera.js
+++ guides-pages/typesCamera.js
@@ -1,8 +1,11 @@
-const url = "https://keadatabase-99c6.restdb.io/rest/types-of-camera?max=2";
+const url = "https://keadatabase-99c6.restdb.io/rest/types-of-camera?max=20";
+const mediaurl = "https://keadatabase-99c6.restdb.io/media/";
+
+// 560263607f98025500000000?s=t
 
 // API KEY 603420035ad3610fb5bb6527
 const options = {
-  header: {
+  headers: {
     "x-apikey": "603420035ad3610fb5bb6527",
   },
 };
@@ -16,16 +19,25 @@ fetch(url, options)
   })
   .then((data) => {
     console.log(data);
-    // handleData(data);
+    handleData(data);
   })
   .catch((e) => {
     console.error("An error occured:", e.message);
   });
 
-// fetch(url)
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     showCameraType(data);
-//   });
+function handleData(data) {
+  data.forEach(showCamera);
+}
+
+function showCamera(camera) {
+  const template = document.querySelector("template").content;
+  const copy = template.cloneNode(true);
+  copy.querySelector("h3").textContent = camera.typeofCamera;
+  copy.querySelector("p").textContent = camera.description;
+
+  const imgurl = mediaurl + camera.image;
+  copy.querySelector("img").src = imgurl;
+
+  const parent = document.querySelector(".typescameraGrid");
+  parent.appendChild(copy);
+}
