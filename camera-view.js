976496f97c40f715d07ec2df284b9f8c676a6ff8
malagorzata <gosia.warwaszynska@gmail.com>
2021-02-23 11:03:13 +0100

    change js

--- camera-view.js
+++ camera-view.js
@@ -1,37 +1,38 @@
-// const urlParams = new URLSearchParams(window.location.search);
-// const id = urlParams.get("_id");
-// const url = "https://web35mm-e688.restdb.io/rest/cameras/" + id;
-// const mediaurl = "https://web35mm-e688.restdb.io/media/";
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("_id");
+const url =
+  "https://web35mm-e688.restdb.io/rest/cameras/602fb277b0bc995a00059a97";
+const mediaurl = "https://web35mm-e688.restdb.io/media";
 
-// /*API key*/
-// const options = {
-//   headers: {
-//     "x-apikey": "603254a35ad3610fb5bb646f",
-//   },
-// };
+/*API key*/
+const options = {
+  headers: {
+    "x-apikey": "603254a35ad3610fb5bb646f",
+  },
+};
 
-// fetch(url, options)
-//   .then((response) => {
-//     if (!response.ok) {
-//       throw Error(response.statusText);
-//     }
-//     return response.json();
-//   })
+fetch(url, options)
+  .then((response) => {
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return response.json();
+  })
 
-//   .then((data) => {
-//     handleCameras(data);
-//   })
-//   .catch((e) => {
-//     console.error("an error occured:", e.message);
-//   });
+  .then((data) => {
+    handleCameras(data);
+  })
+  .catch((e) => {
+    console.error("an error occured:", e.message);
+  });
 
-// function handleCameras(cameras) {
-//   console.log(cameras);
-//   document.querySelector(".cameraName").textContent = cameras.cameraName;
-//   document.querySelector(
-//     ".format-level"
-//   ).textContent = `${cameras.format} | ${cameras.level} `;
-//   document.querySelector(".type").textContent = cameras.type;
-//   document.querySelector(".description").textContent = cameras.description;
-//   document.querySelector("#cameraView h2").textContent = cameras.cameraName;
-// }
+function handleCameras(cameras) {
+  console.log(cameras);
+  document.querySelector(".cameraName").textContent = cameras.cameraName;
+  document.querySelector(
+    ".format-level"
+  ).textContent = `${cameras.format} | ${cameras.level} `;
+  document.querySelector(".type").textContent = cameras.type;
+  document.querySelector(".description").textContent = cameras.description;
+  document.querySelector("#cameraView h2").textContent = cameras.cameraName;
+}
